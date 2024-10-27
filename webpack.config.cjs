1b577f1db58d690e9a7ca794ba8efcc321a7b35c
Antony-Silvesta <antonysilvesta74@gmail.com>
2024-10-27 21:45:25 +0530

    updated fully

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,28 +1,9 @@
-const path = require('path');
-
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.js', // Make sure './src/index.js' exists
     output: {
-        path: path.resolve(__dirname, 'dist'), // Using path module for cross-platform compatibility
-        filename: 'bundle.js',
+      path: __dirname + '/dist',
+      filename: 'bundle.js',
     },
-    mode: 'production', // Set to 'development' for development builds
-    module: {
-        rules: [
-            {
-                test: /\.js$/, // Use Babel to transpile JavaScript files
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-            // You can add more loaders here (e.g., for CSS, images, etc.)
-        ],
-    },
-    plugins: [
-        // You can add plugins here (e.g., HtmlWebpackPlugin for HTML generation)
-    ],
-};
+    // other configurations...
+  };
+  
