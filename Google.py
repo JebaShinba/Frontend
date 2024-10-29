15df5abcb5235b498b0d0c2d7deb119ae568d9b2
JebaShinba <153792090+JebaShinba@users.noreply.github.com>
2024-10-29 19:44:48 +0530

    Update Google.py

--- Google.py
+++ Google.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 
 # Set Chrome options for headless
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
